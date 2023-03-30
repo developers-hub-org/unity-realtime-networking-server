cf7f73c14ad15a8ee9d6fa0c25304dc7d11268f0
Armin <arminahmadi.official@gmail.com>
2023-03-31 01:29:26 +0430

    Added Error Log

--- Scripts/Tools.cs
+++ Scripts/Tools.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 
@@ -7,6 +8,34 @@ namespace DevelopersHub.RealtimeNetworking.Server
 {
     class Tools
     {
+
+        public static readonly string logFolderPath = "C:\\Log\\Realtime Networking\\";
+
+        public static void LogError(string message, string trace, string folder = "")
+        {
+            Console.WriteLine("Error:" + "\n" + message + "\n" + trace);
+            Task task = Task.Run(() =>
+            {
+                try
+                {
+                    string folderPath = logFolderPath;
+                    if (!string.IsNullOrEmpty(folder))
+                    {
+                        folderPath = folderPath + folder + "\\";
+                    }
+                    string path = folderPath + DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss-ffff") + ".txt";
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+                    File.WriteAllText(path, message + "\n" + trace);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error:" + "\n" + ex.Message + "\n" + ex.StackTrace);
+                }
+            });
+        }
 
         public static string GenerateToken()
         {
